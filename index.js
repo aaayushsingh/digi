2c0bf9e58b3620ee19c9fc14086350e776ab937e
aaayushsingh <ayush-_-singh@live.com>
2018-05-16 15:39:25 +0530

    nginx conf at server

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
 app.listen(process.env.port || 3000, (res, err) => {
   console.log("chal raha hai");
 });
